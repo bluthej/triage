71f1a0c55527c5a5313518c854317087ef6f7e5f
bluthej <joffrey.bluthe@e.email>
2023-05-24 13:00:44 +0200

    Move tests back to modules

--- src/bubblesort.rs
+++ src/bubblesort.rs
@@ -11,3 +11,15 @@ pub fn sort<T: Ord>(slice: &mut [T]) {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let mut things = vec![4, 2, 3, 1];
+        sort(&mut things);
+        assert_eq!(&things, &[1, 2, 3, 4]);
+    }
+}
