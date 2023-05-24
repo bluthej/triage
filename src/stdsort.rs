7a0530982951b58c06fa616c38e43104271d02ec
bluthej <joffrey.bluthe@e.email>
2023-05-24 12:41:43 +0200

    Initial commit: setup + std sort with test

---
+++ src/stdsort.rs
@@ -0,0 +1,16 @@
+#[allow(unused)]
+fn sort<T: Ord>(slice: &mut [T]) {
+    slice.sort();
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let mut things = vec![4, 2, 3, 1];
+        sort(&mut things);
+        assert_eq!(&things, &[1, 2, 3, 4]);
+    }
+}
