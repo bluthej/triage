e7d4e44b0159df2fb2546b0055c6ad5003212a75
bluthej <joffrey.bluthe@e.email>
2023-05-24 20:24:36 +0200

    Improve insertion sort
    Use `binary_search` to find the index and `rotate_right` to do the
    shifting

--- src/insertionsort.rs
+++ src/insertionsort.rs
@@ -1,11 +1,10 @@
 #[allow(unused)]
 pub fn sort<T: Ord>(slice: &mut [T]) {
     for unsorted in 1..slice.len() {
-        let mut i = unsorted;
-        while i > 0 && slice[i - 1] > slice[i] {
-            slice.swap(i - 1, i);
-            i -= 1;
-        }
+        let i = slice[..unsorted]
+            .binary_search(&slice[unsorted])
+            .unwrap_or_else(std::convert::identity);
+        slice[i..=unsorted].rotate_right(1);
     }
 }
 
